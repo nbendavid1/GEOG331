17bb7a3f87a342d425d6d645c68ef8d13a979395
nbendavid1016 <70717670+nbendavid1016@users.noreply.github.com>
2020-09-05 14:34:18 -0400

    Finished sample calculations, variables, and vectors.

--- Activity1.R
+++ Activity1.R
@@ -1,1 +1,37 @@
 print("Hello World")
+
+#calculate 4 to the 5th power
+4^5
+
+#1994+543
+1994+543
+
+#26-39
+26-39
+
+#set 579 as "a"
+a <- 579
+
+#calculate my number times 32
+a*32
+
+#my number divided by 4
+a/4
+
+#make a vector of numbers
+b <- c(231,773,1408,12)
+
+#divide all numbers by 3
+b/3
+
+#take the square root of all numbers
+sqrt(b)
+
+#make vector with variable in it
+d <- c(83,346,a,715)
+
+#multiply all numbers by 4
+d*4
+
+#square all numbers
+d^2
